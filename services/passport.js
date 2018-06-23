fdba77b49d9261a4aac0bc6017c7c9fe23cb2b77
John Smith <unknown.prop@gmail.com>
2018-06-23 23:32:48 +0900

    Making an Authenticated Request

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwtl;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 // Setup options for JWT Strategy
 const jwtOptions = {
